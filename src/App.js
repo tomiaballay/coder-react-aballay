f59e4a25f10e992f9e0312ec9c36fa2b02681003
tomiaballay <leandrotomasaba01@gmail.com>
2022-07-05 21:02:52 -0300

    "navbar"

--- src/App.js
+++ src/App.js
@@ -1,25 +1,27 @@
-import logo from './logo.svg';
 import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
 
-function App() {
+function  App () {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      <nav className="navbar navbar-expand-lg navbar-dark bg- letra">
+      <a className="navbar-brand " href="#">M I M A D A S</a>
+      <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+        <span className="navbar-toggler-icon"></span>
+      </button>
+      <div className="collapse navbar-collapse" id="navbarNav">
+        <ul className="navbar-nav">
+          <li className="nav-item active">
+            <a className="nav-link" href="#"> INICIO <span class="sr-only"></span></a>
+          </li>
+          <li className="nav-item">
+            <a className="nav-link" href="#">TIENDA</a>
+          </li>
+          <li className="nav-item">
+            <a className="nav-link" href="#">DUDAS</a>
+          </li>
+        </ul>
+      </div>
+    </nav>
   );
 }
-
 export default App;
